6564511cf13e1dc0d5ee8b69cf9244e9245e395d
Vincent Raulin <vincent.raulin.n7@gmail.com>
2026-02-02 17:34:09 +0100

    Finished first working mod version, with a deepslate bricks portal ignited by a diamond pickaxe!!

---
+++ src/main/java/com/gammahollow/GammaHollow.java
@@ -0,0 +1,34 @@
+package com.gammahollow; // Ensure this matches your folder structure
+
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.kyrptonaught.customportalapi.CustomPortalRegistrationEvent;
+
+@Mod(GammaHollow.MODID)
+public class GammaHollow {
+    public static final String MODID = "gammahollow";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public GammaHollow(IEventBus modEventBus) {
+        modEventBus.addListener(this::registerPortals);
+    }
+
+    private void registerPortals(CustomPortalRegistrationEvent event) {
+        CustomPortalBuilder.beginPortal()
+            .frameBlock(Blocks.DEEPSLATE_BRICKS)
+            .lightWithItem(Items.DIAMOND_PICKAXE)
+            .destDimID(ResourceLocation.fromNamespaceAndPath(MODID, "gamma"))
+            .onlyLightInOverworld()
+            .tintColor(100, 50, 200)
+            .registerPortal();
+        LOGGER.info("Gamma Hollow Portals Registered during Common Setup!");
+    }
+}
